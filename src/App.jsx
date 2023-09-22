73fa84935fa08f37b5b66e6f1db46c7aa32c46a6
Tiago Silva <tiagobsb84@gmail.com>
2023-09-22 15:19:13 -0300

    adicionando componente banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { styled } from 'styled-components';
 import EstiloGlobais from './components/EstiloGlobais';
 import Cabecalho from './components/Cabecalho';
 import BarraLateral from './components/BarraLateral';
+import Banner from './components/Banner';
+
+import bannerBackground from './assets/banner.png'
 
 const FundoGradiente = styled.div`
   background: var(--Gradiente-fundo, linear-gradient(175deg, #041833 4.16%, #04244F 48%, #154580 96.76%));
@@ -16,6 +19,10 @@ function App() {
       <EstiloGlobais />
       <Cabecalho />
       <BarraLateral />
+      <Banner  
+        texto='A galeria mais completa de fotos do espaço!'
+        backgroudImage={bannerBackground}
+      />
     </FundoGradiente>
   )
 }
---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+const FiguraPersonalizado = styled.figure`
+    background-image: ${props => `url(${props.$backgroudImage})`};
+    flex-grow: 1;
+    background-repeat: no-repeat;
+    display: flex;
+    align-items: center;
+    min-height: 328px;
+    margin: 0;
+    border-radius: 20px;
+    max-width: 100%;
+`;
+
+const TituloEstilizado = styled.h1`
+    font-weight: 400;
+    font-size: 40px;
+    line-height: 48px;
+    color: #FFFFFF;
+    max-width: 300px;
+    padding: 0 64px;
+`;
+
+const Banner = ({texto, backgroudImage}) => {
+    return(
+        <FiguraPersonalizado $backgroudImage={backgroudImage}>
+            <TituloEstilizado>{texto}</TituloEstilizado>
+        </FiguraPersonalizado>
+    )
+}
+
+export default Banner;
